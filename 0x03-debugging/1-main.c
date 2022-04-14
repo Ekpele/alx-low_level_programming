104f13750966f332b15469c9fb1ffea17acdb516
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-04-14 05:17:43 -0700

    1 main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,9 +9,7 @@ int main(void)
 int i;
 
 printf("infinite loop incoming :(\n');
-
 i = 0;
-
 
 /*
  *while (i < 10)
@@ -20,7 +18,6 @@ i = 0;
  *putchar(i);
 }
 */
-
 printf("infinite loop avoided! \\o/\n");
 return (0);
 }
