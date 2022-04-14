47718358379ccd0c41c0fd60679e0146d96a9958
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-04-14 12:05:51 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_numbers - print numbers
  * Return: Always 0.
@@ -8,9 +9,10 @@ void print_numbers(void)
 int i;
 
 i = 0;
+
 while (i < 10)
 {
-_putchar (i ="0");
+_putchar (i + '0');
 i++;
 }
 
