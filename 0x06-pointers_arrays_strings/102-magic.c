165b13729f275a9f28ea0b0b8071bf7658752033
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-04-24 02:24:53 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,7 +6,9 @@ int main(void)
   int n;
   int a[5];
   int *p;
+
   a[2] = 1024;
+
   p = &n;
 
   /*
@@ -18,10 +20,11 @@ int main(void)
    * - you are not allowed to code anything else than this line of code
    */
 
- p[5] = 98;
+  p[5] = 98;
 
   /* ...so that this prints 98\n */
 
   printf("a[2] = %d\n", a[2]);
+
   return (0);
 }
