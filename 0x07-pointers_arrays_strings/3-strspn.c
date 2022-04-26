0ba370ed33cdf0bc784830b9a63179eb4470beb2
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-04-26 04:57:07 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,44 +1,37 @@
 #include "main.h"
 
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
+ * _strspn - get length of a prefix substring
+ * @s: string
+ * @accept: bytes being compared
+ * Description: finds number of bytes required to find accept in s
+ * Return: bytes required to find accept
  **/
 
 unsigned int _strspn(char *s, char *accept)
 
 {
-	int i, j;
-	int count = 0;
-	char *str1, *str2;
-	str1 = s;
-	str2 = accept;
-	i = 0;
+	int i, j, exit;
 
-	while (str1[i] != '\0') /*Declaring WHILE *s */
+	for (i = 0; s[i] != '\0'; i++)
 
 	{
-		j = 0;
-
-		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+		exit = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 
 		{
-			if (str2[j] == str1[i]) /*Evaluate condition*/
+			if (accept[j] == s[i])
+
 			{
-				count++; /*count number*/
-				break;
+				exit = 1;
 			}
-			j++;    /*add j+1*/
 		}
 
-		if (s[i] != accept[j]) /*If aren't equals*/
+		if (exit == 0)
 
 		{
-			break;
+			return (i);
 		}
-		i++; /*add x+1*/
 	}
-	return (count); /*return the value of count*/
+	return (i);
 }
