f129e000782f809542b0b87fcc81e517e37c8054
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-05-09 04:29:31 -0700

    mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,40 +1,17 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
- */
+ * create_array -
+ * @size: size of array
+ * @c: char to assign
+ * Description:
+ * Return:
+ **/
 
-int main(int argc, char *argv[])
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
-unsigned long mul;
-int i, j;
-
-	if (argc != 3)
-
-	{ printf("Error\n");
-
-	exit(98); }
-
-	for (i = 1; i < argc; i++)
-
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-
-			{  printf("Error\n");
-
-			exit(98); }
-		}
-	}
-
-	mul = atol(argv[1]) *atol(argv[2]);
-	printf("%lu\n", mul);
-
-return (0);
+	return (0);
 }
