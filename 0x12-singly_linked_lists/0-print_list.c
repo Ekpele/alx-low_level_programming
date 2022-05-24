426f2f87b839a088f0c22cf3560bd9175ee49bd6
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-05-24 04:38:25 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,19 +11,10 @@
 size_t print_list(const list_t *h)
 
 {
-
 	if (h == NULL)
-
 		return (0);
-
 	printf("[%u] %s\n", h->len, h->str);
-
-
-
 	if (h->next != NULL)
-
 		return (print_list(h->next) + 1);
-
 	return (1);
-
 }
