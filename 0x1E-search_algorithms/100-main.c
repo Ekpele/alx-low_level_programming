ef194f1b4ff1b5a3f2d96c1f146ad2617b0d8d09
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-11-02 12:58:54 -0700

    100-main.c

--- 0x1E-search_algorithms/100-main.c
+++ 0x1E-search_algorithms/100-main.c
@@ -9,8 +9,10 @@
 int main(void)
 {
     int array[] = {
+
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     };
+
     size_t size = sizeof(array) / sizeof(array[0]);
 
     printf("Found %d at index: %d\n\n", 6, jump_search(array, size, 6));
